469ee597c55b8549f15e56f9957e5a8c5ea03fec
Xzhyan <malcondev.lds@hotmail.com>
2026-05-11 20:45:13 -0300

    removed instaloader

--- tests/main.py
+++ tests/main.py
@@ -1,33 +1,14 @@
-import instaloader
 
-
-def post_download(loader, url):
-    post = instaloader.Post.from_shortcode(
-        loader.context,
-        url
-    )
-
-    try:
-        loader.download_post(post, target='posts')
-
-    except Exception as e:
-        print(e)
 
 
 class Main:
     def __init__(self):
-        self.loader = instaloader.Instaloader()
-
-        self.dispatch()
-
-    def dispatch(self):
-            entry = input(" > ")
-
-            if entry:
-                post_download(self.loader, entry)
-        
+        pass
 
 
 if __name__ == '__main__':
-    Main()
+    try:
+        Main()
 
+    except KeyboardInterrupt:
+        print('interrompido')
